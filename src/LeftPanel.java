a63b53489811092d568c8f20e6f6bd06a13ac627
antonioyaghi <antonioyaghi@Antonios-MacBook-Pro.local>
2017-04-24 21:49:33 +0300

    some stuff are still messy!!

--- src/LeftPanel.java
+++ src/LeftPanel.java
@@ -1,7 +1,16 @@
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JSpinner;
+import javax.swing.JToggleButton;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.SpringLayout;
 
 public class LeftPanel extends JPanel {
@@ -13,33 +22,58 @@ public class LeftPanel extends JPanel {
 		SpringLayout springLayout = new SpringLayout();
 		setLayout(springLayout);
 		
-		
-		JButton btnNewButton = new JButton("New button");
-		springLayout.putConstraint(SpringLayout.WEST, btnNewButton, 32, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, btnNewButton, -38, SpringLayout.SOUTH, this);
-		btnNewButton.setBounds(28, 266, 89, 23);
-		add(btnNewButton);
+		setPreferredSize(new Dimension(185,306));
+		setVisible(true);
+		setBackground(SystemColor.window);
 		
 		JSpinner spinner = new JSpinner();
-		springLayout.putConstraint(SpringLayout.WEST, spinner, 49, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.EAST, spinner, -21, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.WEST, spinner, 111, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, spinner, -131, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, spinner, -217, SpringLayout.EAST, this);
 		spinner.setBounds(55, 11, 62, 20);
 		add(spinner);
 		
 		JSpinner spinner_1 = new JSpinner();
-		springLayout.putConstraint(SpringLayout.NORTH, spinner_1, 25, SpringLayout.SOUTH, spinner);
-		springLayout.putConstraint(SpringLayout.WEST, spinner_1, 0, SpringLayout.WEST, spinner);
-		springLayout.putConstraint(SpringLayout.EAST, spinner_1, -21, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, spinner_1, 6, SpringLayout.SOUTH, spinner);
+		springLayout.putConstraint(SpringLayout.WEST, spinner_1, 100, SpringLayout.WEST, this);
 		spinner_1.setBounds(55, 54, 62, 20);
 		add(spinner_1);
 		
 		JSpinner spinner_2 = new JSpinner();
-		springLayout.putConstraint(SpringLayout.WEST, spinner_2, 49, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, spinner_2, -226, SpringLayout.SOUTH, this);
-		springLayout.putConstraint(SpringLayout.EAST, spinner_2, -21, SpringLayout.EAST, this);
-		springLayout.putConstraint(SpringLayout.NORTH, spinner, 17, SpringLayout.SOUTH, spinner_2);
+		springLayout.putConstraint(SpringLayout.NORTH, spinner_2, 17, SpringLayout.SOUTH, spinner_1);
+		springLayout.putConstraint(SpringLayout.WEST, spinner_2, 89, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, spinner_2, -200, SpringLayout.EAST, this);
 		spinner_2.setBounds(55, 98, 62, 20);
 		add(spinner_2);
 
+		JLabel Operations = new JLabel("OPERATIONS");
+		springLayout.putConstraint(SpringLayout.NORTH, Operations, 17, SpringLayout.SOUTH, spinner);
+		springLayout.putConstraint(SpringLayout.WEST, Operations, 66, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, Operations, -11, SpringLayout.NORTH, spinner_1);
+		springLayout.putConstraint(SpringLayout.EAST, Operations, -39, SpringLayout.EAST, this);
+		add(Operations);
+		
+		JButton add_btn = new JButton("Add Points");
+		springLayout.putConstraint(SpringLayout.EAST, spinner_1, -37, SpringLayout.EAST, add_btn);
+		springLayout.putConstraint(SpringLayout.SOUTH, add_btn, -258, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, add_btn, 26, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, add_btn, -178, SpringLayout.EAST, this);
+		add_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		add(add_btn);
+		
+		JButton btnMultiplyPointBy = new JButton("Multiply Point by");
+		springLayout.putConstraint(SpringLayout.SOUTH, btnMultiplyPointBy, -58, SpringLayout.NORTH, Operations);
+		springLayout.putConstraint(SpringLayout.EAST, btnMultiplyPointBy, -10, SpringLayout.EAST, this);
+		add(btnMultiplyPointBy);
+		
+		JToggleButton select_btn = new JToggleButton("Select Points");
+		springLayout.putConstraint(SpringLayout.WEST, select_btn, 0, SpringLayout.WEST, Operations);
+		springLayout.putConstraint(SpringLayout.SOUTH, select_btn, -58, SpringLayout.NORTH, spinner);
+		springLayout.putConstraint(SpringLayout.EAST, select_btn, -184, SpringLayout.EAST, this);
+		add(select_btn);
+		
 	}
 }
--- src/TestWindow.java
+++ src/TestWindow.java
@@ -1,15 +1,36 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import java.awt.SystemColor;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JToggleButton;
 
 public class TestWindow {
 
 	private JFrame frame;
-	private CartesianPlane plane;
+	private CartesianPlane graph;
 	private EllipticCurve c1;
+	private JPanel right_panel;
+	private JPanel upper_panel;
+	private JPanel lower_panel;
+	private JMenuBar menuBar;
+	private JMenu Menu;
+	private JLayeredPane layeredPane;
+	private JLabel Operations;
 
 	/**
 	 * Launch the application.
@@ -39,28 +60,62 @@ public class TestWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 570, 300);
+		frame.setBounds(100, 100, 1000, 612);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		plane = new CartesianPlane(frame.getWidth(), frame.getHeight());
+		graph = new CartesianPlane(frame.getWidth(), frame.getHeight());
 		
 		c1 = new EllipticCurve(5);
 		c1.setA(4);
 		c1.setB(4);
 		
 		
-		plane.setCurve(c1);
+		graph.setCurve(c1);
 		
-		LeftPanel p = new LeftPanel();
-		p.setPreferredSize(new Dimension(150,450));
-	
-		p.setVisible(true);
+		upper_panel = new JPanel();
+		frame.getContentPane().add(upper_panel, BorderLayout.NORTH);
+		upper_panel.setPreferredSize(new Dimension(50, 50));
+		upper_panel.setVisible(true);
+		upper_panel.setBackground(SystemColor.window);
 		
-		//this.getContentPane().add(buttonPane,BorderLayout.SOUTH);
-		frame.getContentPane().add(p,BorderLayout.WEST); //Set content pannel
-		frame.getContentPane().add(plane, BorderLayout.CENTER);
+		lower_panel = new JPanel();
+		frame.getContentPane().add(lower_panel, BorderLayout.SOUTH);
+		lower_panel.setPreferredSize(new Dimension(50,50));
+		lower_panel.setVisible(true);
+		lower_panel.setBackground(SystemColor.window);
+		
+		LeftPanel left_panel = new LeftPanel();
+		left_panel.setPreferredSize(new Dimension(185,306));
+		
+		//SpringLayout springLayout = (SpringLayout) left_panel.getLayout();
+		//frame.getContentPane().add(left_panel,BorderLayout.WEST);
+		
+		
+		
+		right_panel = new JPanel();
+		frame.getContentPane().add(right_panel, BorderLayout.EAST);
+		right_panel.setPreferredSize(new Dimension(185,306));
+		right_panel.setVisible(true);
+		right_panel.setBackground(SystemColor.window);
+		
+		frame.getContentPane().add(graph, BorderLayout.CENTER);
+		graph.setPreferredSize(new Dimension(500,500));
+		
+		menuBar = new JMenuBar();
+		menuBar.setBackground(new Color(204, 0, 255));
+		frame.setJMenuBar(menuBar);
+		menuBar.setVisible(true);
+		
+		Menu = new JMenu("New menu");
+		menuBar.add(Menu);
+		
+		JMenuItem mntmEllipticCurve = new JMenuItem("Elliptic Curve");
+		Menu.add(mntmEllipticCurve);
+		
+		JMenuItem mntmSurprise = new JMenuItem("Surprise!");
+		Menu.add(mntmSurprise);
+		graph.setVisible(true);
 		frame.getContentPane().setVisible(true);
 	
 	}
-
 }
