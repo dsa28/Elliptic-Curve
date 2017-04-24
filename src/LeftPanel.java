3c1c129261c441bce89ae5da8a1f681d6a6d0964
antonioyaghi <antonioyaghi@Antonios-MacBook-Pro.local>
2017-04-24 21:53:24 +0300

    small modifications

--- src/LeftPanel.java
+++ src/LeftPanel.java
@@ -25,55 +25,67 @@ public class LeftPanel extends JPanel {
 		setPreferredSize(new Dimension(185,306));
 		setVisible(true);
 		setBackground(SystemColor.window);
-		
-		JSpinner spinner = new JSpinner();
-		springLayout.putConstraint(SpringLayout.WEST, spinner, 111, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, spinner, -131, SpringLayout.SOUTH, this);
-		springLayout.putConstraint(SpringLayout.EAST, spinner, -217, SpringLayout.EAST, this);
-		spinner.setBounds(55, 11, 62, 20);
-		add(spinner);
-		
-		JSpinner spinner_1 = new JSpinner();
-		springLayout.putConstraint(SpringLayout.NORTH, spinner_1, 6, SpringLayout.SOUTH, spinner);
-		springLayout.putConstraint(SpringLayout.WEST, spinner_1, 100, SpringLayout.WEST, this);
-		spinner_1.setBounds(55, 54, 62, 20);
-		add(spinner_1);
-		
-		JSpinner spinner_2 = new JSpinner();
-		springLayout.putConstraint(SpringLayout.NORTH, spinner_2, 17, SpringLayout.SOUTH, spinner_1);
-		springLayout.putConstraint(SpringLayout.WEST, spinner_2, 89, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.EAST, spinner_2, -200, SpringLayout.EAST, this);
-		spinner_2.setBounds(55, 98, 62, 20);
-		add(spinner_2);
 
 		JLabel Operations = new JLabel("OPERATIONS");
-		springLayout.putConstraint(SpringLayout.NORTH, Operations, 17, SpringLayout.SOUTH, spinner);
 		springLayout.putConstraint(SpringLayout.WEST, Operations, 66, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, Operations, -11, SpringLayout.NORTH, spinner_1);
 		springLayout.putConstraint(SpringLayout.EAST, Operations, -39, SpringLayout.EAST, this);
 		add(Operations);
 		
-		JButton add_btn = new JButton("Add Points");
-		springLayout.putConstraint(SpringLayout.EAST, spinner_1, -37, SpringLayout.EAST, add_btn);
-		springLayout.putConstraint(SpringLayout.SOUTH, add_btn, -258, SpringLayout.SOUTH, this);
-		springLayout.putConstraint(SpringLayout.WEST, add_btn, 26, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.EAST, add_btn, -178, SpringLayout.EAST, this);
-		add_btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		add(add_btn);
-		
 		JButton btnMultiplyPointBy = new JButton("Multiply Point by");
-		springLayout.putConstraint(SpringLayout.SOUTH, btnMultiplyPointBy, -58, SpringLayout.NORTH, Operations);
-		springLayout.putConstraint(SpringLayout.EAST, btnMultiplyPointBy, -10, SpringLayout.EAST, this);
 		add(btnMultiplyPointBy);
 		
-		JToggleButton select_btn = new JToggleButton("Select Points");
-		springLayout.putConstraint(SpringLayout.WEST, select_btn, 0, SpringLayout.WEST, Operations);
-		springLayout.putConstraint(SpringLayout.SOUTH, select_btn, -58, SpringLayout.NORTH, spinner);
-		springLayout.putConstraint(SpringLayout.EAST, select_btn, -184, SpringLayout.EAST, this);
-		add(select_btn);
+		JSpinner spinner = new JSpinner();
+		springLayout.putConstraint(SpringLayout.EAST, spinner, -13, SpringLayout.EAST, this);
+		add(spinner);
+		
+		JSpinner spinner_1 = new JSpinner();
+		springLayout.putConstraint(SpringLayout.NORTH, spinner, 6, SpringLayout.SOUTH, spinner_1);
+		springLayout.putConstraint(SpringLayout.WEST, spinner_1, 0, SpringLayout.WEST, Operations);
+		springLayout.putConstraint(SpringLayout.EAST, spinner_1, -12, SpringLayout.EAST, this);
+		add(spinner_1);
+		
+		JSpinner spinner_2 = new JSpinner();
+		springLayout.putConstraint(SpringLayout.EAST, spinner_2, -13, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, spinner_1, 6, SpringLayout.SOUTH, spinner_2);
+		add(spinner_2);
+		
+		JButton btnAddPoints = new JButton("Add Points");
+		springLayout.putConstraint(SpringLayout.EAST, btnAddPoints, 0, SpringLayout.EAST, Operations);
+		add(btnAddPoints);
+		
+		JToggleButton btnSelectPoints = new JToggleButton("Select Points");
+		springLayout.putConstraint(SpringLayout.WEST, btnSelectPoints, 22, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, btnMultiplyPointBy, 6, SpringLayout.SOUTH, btnSelectPoints);
+		springLayout.putConstraint(SpringLayout.WEST, btnMultiplyPointBy, 0, SpringLayout.WEST, btnSelectPoints);
+		springLayout.putConstraint(SpringLayout.NORTH, Operations, 45, SpringLayout.SOUTH, btnSelectPoints);
+		springLayout.putConstraint(SpringLayout.SOUTH, Operations, -74, SpringLayout.NORTH, btnSelectPoints);
+		springLayout.putConstraint(SpringLayout.NORTH, btnSelectPoints, 39, SpringLayout.SOUTH, spinner);
+		add(btnSelectPoints);
+		
+		JLabel lblSelectA = new JLabel("Select A");
+		springLayout.putConstraint(SpringLayout.EAST, lblSelectA, -123, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.WEST, spinner_2, 3, SpringLayout.EAST, lblSelectA);
+		springLayout.putConstraint(SpringLayout.NORTH, spinner_2, -5, SpringLayout.NORTH, lblSelectA);
+		springLayout.putConstraint(SpringLayout.NORTH, lblSelectA, 10, SpringLayout.NORTH, this);
+		add(lblSelectA);
+		
+		JLabel lblSelectB = new JLabel("Select B");
+		springLayout.putConstraint(SpringLayout.NORTH, lblSelectB, 5, SpringLayout.NORTH, spinner_1);
+		springLayout.putConstraint(SpringLayout.EAST, lblSelectB, -6, SpringLayout.WEST, spinner_1);
+		add(lblSelectB);
+		
+		JLabel lblSelectP = new JLabel("Select P");
+		springLayout.putConstraint(SpringLayout.EAST, lblSelectP, -126, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.WEST, spinner, 6, SpringLayout.EAST, lblSelectP);
+		springLayout.putConstraint(SpringLayout.NORTH, lblSelectP, 5, SpringLayout.NORTH, spinner);
+		add(lblSelectP);
+		
+		JSpinner spinner_3 = new JSpinner();
+		springLayout.putConstraint(SpringLayout.NORTH, btnAddPoints, 6, SpringLayout.SOUTH, spinner_3);
+		springLayout.putConstraint(SpringLayout.NORTH, spinner_3, 6, SpringLayout.SOUTH, btnMultiplyPointBy);
+		springLayout.putConstraint(SpringLayout.WEST, spinner_3, 32, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, spinner_3, 111, SpringLayout.WEST, btnMultiplyPointBy);
+		add(spinner_3);
 		
 	}
 }
