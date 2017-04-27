4be6d61e1b58faa26e28567ecea0e5af10da3f8e
antonioyaghi <antonioyaghi@Antonios-MacBook-Pro.local>
2017-04-27 11:16:26 +0300

    PFFF

--- src/CenterPanel2.java
+++ src/CenterPanel2.java
@@ -26,71 +26,67 @@ public class CenterPanel2 extends JPanel {
 		
 		JButton btnReduceP1 = new JButton("Reduce P1");
 		springLayout.putConstraint(SpringLayout.NORTH, btnReduceP1, 71, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.WEST, btnReduceP1, 187, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.WEST, btnReduceP1, 244, SpringLayout.WEST, this);
 		add(btnReduceP1);
 		
 		JButton btnReduceP2 = new JButton("Reduce P2");
-		springLayout.putConstraint(SpringLayout.EAST, btnReduceP1, -24, SpringLayout.WEST, btnReduceP2);
-		springLayout.putConstraint(SpringLayout.EAST, btnReduceP2, 485, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.WEST, btnReduceP2, 348, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, btnReduceP2, 6, SpringLayout.SOUTH, btnReduceP1);
+		springLayout.putConstraint(SpringLayout.WEST, btnReduceP2, 244, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, btnReduceP2, 0, SpringLayout.EAST, btnReduceP1);
 		add(btnReduceP2);
 		
 		JButton btnInverseOfP1 = new JButton("Inverse of P1");
-		springLayout.putConstraint(SpringLayout.WEST, btnInverseOfP1, 24, SpringLayout.EAST, btnReduceP2);
-		springLayout.putConstraint(SpringLayout.EAST, btnInverseOfP1, -377, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, btnInverseOfP1, 6, SpringLayout.SOUTH, btnReduceP2);
+		springLayout.putConstraint(SpringLayout.WEST, btnInverseOfP1, 244, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, btnInverseOfP1, 0, SpringLayout.EAST, btnReduceP1);
 		add(btnInverseOfP1);
 		
 		JButton btnInverseOfP2 = new JButton("Inverse of P2");
-		springLayout.putConstraint(SpringLayout.NORTH, btnInverseOfP2, 0, SpringLayout.NORTH, btnReduceP1);
-		springLayout.putConstraint(SpringLayout.WEST, btnInverseOfP2, 6, SpringLayout.EAST, btnInverseOfP1);
-		springLayout.putConstraint(SpringLayout.EAST, btnInverseOfP2, -217, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, btnInverseOfP2, 6, SpringLayout.SOUTH, btnInverseOfP1);
+		springLayout.putConstraint(SpringLayout.WEST, btnInverseOfP2, 244, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, btnInverseOfP2, 0, SpringLayout.EAST, btnReduceP1);
 		add(btnInverseOfP2);
 		
 		JButton btnAdd = new JButton("P1 + P2");
-		springLayout.putConstraint(SpringLayout.WEST, btnAdd, 18, SpringLayout.WEST, btnReduceP1);
-		springLayout.putConstraint(SpringLayout.SOUTH, btnAdd, 94, SpringLayout.SOUTH, btnReduceP1);
+		springLayout.putConstraint(SpringLayout.EAST, btnReduceP1, -33, SpringLayout.WEST, btnAdd);
+		springLayout.putConstraint(SpringLayout.WEST, btnAdd, 414, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, btnAdd, 551, SpringLayout.WEST, this);
 		add(btnAdd);
 		
 		JButton btnSubtract = new JButton("P1 - P2");
-		springLayout.putConstraint(SpringLayout.NORTH, btnAdd, 0, SpringLayout.NORTH, btnSubtract);
-		springLayout.putConstraint(SpringLayout.EAST, btnAdd, -39, SpringLayout.WEST, btnSubtract);
-		springLayout.putConstraint(SpringLayout.NORTH, btnSubtract, 23, SpringLayout.SOUTH, btnReduceP1);
-		springLayout.putConstraint(SpringLayout.WEST, btnSubtract, 315, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, btnSubtract, 94, SpringLayout.SOUTH, btnReduceP1);
+		springLayout.putConstraint(SpringLayout.NORTH, btnSubtract, 6, SpringLayout.SOUTH, btnAdd);
+		springLayout.putConstraint(SpringLayout.WEST, btnSubtract, 33, SpringLayout.EAST, btnReduceP2);
+		springLayout.putConstraint(SpringLayout.EAST, btnSubtract, 170, SpringLayout.EAST, btnReduceP2);
 		add(btnSubtract);
 		
 		JButton btnSubtractt = new JButton("P2 - P1");
-		springLayout.putConstraint(SpringLayout.EAST, btnSubtract, -28, SpringLayout.WEST, btnSubtractt);
-		springLayout.putConstraint(SpringLayout.NORTH, btnSubtractt, 23, SpringLayout.SOUTH, btnReduceP2);
-		springLayout.putConstraint(SpringLayout.WEST, btnSubtractt, 414, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, btnSubtractt, 94, SpringLayout.SOUTH, btnReduceP2);
-		springLayout.putConstraint(SpringLayout.EAST, btnSubtractt, 485, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, btnSubtractt, 106, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, btnSubtractt, 574, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, btnSubtractt, 711, SpringLayout.WEST, this);
 		add(btnSubtractt);
 		
 		JButton btnMultiply = new JButton("P1 x P2");
-		springLayout.putConstraint(SpringLayout.NORTH, btnMultiply, 23, SpringLayout.SOUTH, btnInverseOfP1);
-		springLayout.putConstraint(SpringLayout.WEST, btnMultiply, 28, SpringLayout.EAST, btnSubtractt);
-		springLayout.putConstraint(SpringLayout.SOUTH, btnMultiply, 94, SpringLayout.SOUTH, btnInverseOfP1);
-		springLayout.putConstraint(SpringLayout.EAST, btnMultiply, 99, SpringLayout.EAST, btnSubtractt);
+		springLayout.putConstraint(SpringLayout.NORTH, btnMultiply, 9, SpringLayout.SOUTH, btnSubtract);
+		springLayout.putConstraint(SpringLayout.WEST, btnMultiply, 33, SpringLayout.EAST, btnInverseOfP1);
+		springLayout.putConstraint(SpringLayout.EAST, btnMultiply, 170, SpringLayout.EAST, btnInverseOfP1);
 		add(btnMultiply);
 		
-		JButton btnDivide = new JButton("P1 ÷ P2");
-		springLayout.putConstraint(SpringLayout.NORTH, btnDivide, 23, SpringLayout.SOUTH, btnInverseOfP2);
-		springLayout.putConstraint(SpringLayout.WEST, btnDivide, 40, SpringLayout.EAST, btnMultiply);
-		springLayout.putConstraint(SpringLayout.SOUTH, btnDivide, 94, SpringLayout.SOUTH, btnInverseOfP2);
-		springLayout.putConstraint(SpringLayout.EAST, btnDivide, 111, SpringLayout.EAST, btnMultiply);
+		JButton btnDivide = new JButton("P1 / P2");
+		springLayout.putConstraint(SpringLayout.SOUTH, btnMultiply, -3, SpringLayout.NORTH, btnDivide);
+		springLayout.putConstraint(SpringLayout.WEST, btnDivide, 33, SpringLayout.EAST, btnInverseOfP2);
+		springLayout.putConstraint(SpringLayout.EAST, btnDivide, 170, SpringLayout.EAST, btnInverseOfP2);
+		springLayout.putConstraint(SpringLayout.NORTH, btnDivide, 0, SpringLayout.NORTH, btnInverseOfP2);
 		add(btnDivide);
 		
-		JButton btnDividee = new JButton("P2 ÷ P1");
-		springLayout.putConstraint(SpringLayout.NORTH, btnDividee, 23, SpringLayout.SOUTH, btnInverseOfP2);
-		springLayout.putConstraint(SpringLayout.WEST, btnDividee, 36, SpringLayout.EAST, btnDivide);
-		springLayout.putConstraint(SpringLayout.SOUTH, btnDividee, 94, SpringLayout.SOUTH, btnInverseOfP2);
-		springLayout.putConstraint(SpringLayout.EAST, btnDividee, 107, SpringLayout.EAST, btnDivide);
+		JButton btnDividee = new JButton("P2 / P1");
+		springLayout.putConstraint(SpringLayout.NORTH, btnDividee, 0, SpringLayout.NORTH, btnInverseOfP2);
+		springLayout.putConstraint(SpringLayout.WEST, btnDividee, 23, SpringLayout.EAST, btnDivide);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnDividee, 0, SpringLayout.SOUTH, btnInverseOfP2);
+		springLayout.putConstraint(SpringLayout.EAST, btnDividee, 160, SpringLayout.EAST, btnDivide);
 		add(btnDividee);
 		
 		JLabel btnSubtract1 = new JLabel("OPERATIONS");
-		springLayout.putConstraint(SpringLayout.NORTH, btnInverseOfP1, 40, SpringLayout.SOUTH, btnSubtract1);
-		springLayout.putConstraint(SpringLayout.NORTH, btnReduceP2, 40, SpringLayout.SOUTH, btnSubtract1);
+		springLayout.putConstraint(SpringLayout.NORTH, btnAdd, 40, SpringLayout.SOUTH, btnSubtract1);
 		springLayout.putConstraint(SpringLayout.NORTH, btnSubtract1, 10, SpringLayout.NORTH, this);
 		springLayout.putConstraint(SpringLayout.WEST, btnSubtract1, 437, SpringLayout.WEST, this);
 		btnSubtract1.setFont(new Font("Lucida Grande", Font.BOLD, 17));
